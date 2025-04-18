14fc7cc1dfbd0befac0e65f07011ccbd491284dc
Tamtai <107045052+Tainyz@users.noreply.github.com>
2025-04-18 17:43:18 +0700

    backend finish

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 
+import dotenv from 'dotenv';
+import productRoutes from './routes/product.route.js';
 
+dotenv.config();
 const app = express();
 
+app.use(express.json()); //allow express to parse json data
 
-app.get('/products', (req, res) => {    
-    res.send('Hello World');
-});
-
+app.use("/api/products", productRoutes); //use product routes
 
 app.listen(5000,() => {
     connectDB();
